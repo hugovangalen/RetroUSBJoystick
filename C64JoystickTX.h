f0298508dd1d2a6e4b96ca83eb2f8c66fc08af86
Hugo van Galen <hugo@wellevue.com>
2020-01-09 16:05:21 +0100

    Added counters.

--- C64JoystickTX.h
+++ C64JoystickTX.h
@@ -10,12 +10,21 @@
 #include "C64JoystickBase.h"
 #include "C64JoystickPacket.h"
 
+/**
+ * If `C64JOYSTICK_TX_TEST` is enabled, the loop() will
+ * infinitely iterate all the buttons and press/release
+ * them with a delay.
+ */
+//#define C64JOYSTICK_TX_TEST
 
 class C64JoystickTX : public NRFLite, C64JoystickBase {
     private:
         uint8_t _deviceId;
         JoystickPacket _packet;
         
+        uint32_t _txCount = 0;
+        uint32_t _failedTx = 0;
+
     public:
         C64JoystickTX( uint8_t deviceId );
         
