f7c0a5fdc3255352497cbc53374ce96ae676f121
Hugo van Galen <hugo@wellevue.com>
2020-01-09 10:14:22 +0100

    Moved structure to separate file.

---
+++ C64JoystickPacket.h
@@ -0,0 +1,12 @@
+#ifndef _C64JOYSTICK_PACKET_H_
+#define _C64JOYSTICK_PACKET_H_
+
+struct JoystickPacket
+{
+    uint8_t  id;    // client identification (radio ID)
+    uint32_t seq;   // sequence number
+
+    uint8_t  buttonState;
+};
+
+#endif /* _C64JOYSTICK_PACKET_H_ */
