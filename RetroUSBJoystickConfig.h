1a7e3bf530e1d14b2eadcffa26916f43ca737e43
Hugo van Galen <hugo@wellevue.com>
2020-01-22 14:13:26 +0100

    Do not define `REMOTE_ENCRYPTION_KEY` by default.

--- RetroUSBJoystickConfig.h
+++ RetroUSBJoystickConfig.h
@@ -49,10 +49,10 @@
  * This is purely to prevent receiving packets intended for 
  * another device -- this does *not* prevent replay attacks!
  */
-#define REMOTE_ENCRYPTION_KEY   "\x01\x02\x03\x04\x05\x06\x07\x08"\
-                                "\x01\x02\x03\x04\x05\x06\x07\x08"\
-                                "\x01\x02\x03\x04\x05\x06\x07\x08"\
-                                "\x01\x02\x03\x04\x05\x06\x07\x08"
+//#define REMOTE_ENCRYPTION_KEY   "\x01\x02\x03\x04\x05\x06\x07\x08"\
+//                                "\x01\x02\x03\x04\x05\x06\x07\x08"\
+//                                "\x01\x02\x03\x04\x05\x06\x07\x08"\
+//                                "\x01\x02\x03\x04\x05\x06\x07\x08"
                                 
 
 /** 
